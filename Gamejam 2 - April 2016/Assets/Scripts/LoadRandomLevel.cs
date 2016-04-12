4fcb8f7f193e75763a9445b5c305f874455a6415
Benjamin Moore <benjamin.moore@ad.aie.edu>
2016-04-12 11:49:04 +1000

    Added Base for loading Random Level

---
+++ Gamejam 2 - April 2016/Assets/Scripts/LoadRandomLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadRandomLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void loadRandomLevel()
+	{
+		SceneManager.LoadScene(Random.Range(2,SceneManager.sceneCount));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
