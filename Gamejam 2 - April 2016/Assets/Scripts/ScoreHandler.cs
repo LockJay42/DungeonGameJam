6005a3aed66552fe4025137bc93fd96c6e45f50c
Lachlan Jordon <lachlan.jordon@ad.aie.edu>
2016-04-12 15:18:42 +1000

    why does sourcetree not want me to merge scores

---
+++ Gamejam 2 - April 2016/Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour {
+
+    public Text playerOneScore;
+    public Text playerTwoScore;
+    public Text playerThreeScore;
+    public Text dungeonMasterScore;
+
+    public PlayerScript playerOne;
+    public PlayerScript playerTwo;
+    public PlayerScript playerThree;
+    public DungeonMaster dungeonMaster;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerOneScore.text = "0";
+        playerTwoScore.text = "0";
+        playerThreeScore.text = "0";
+        dungeonMasterScore.text = "0";
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        playerOneScore.text = playerOne.score.ToString();
+        playerTwoScore.text = playerTwo.score.ToString();
+        playerThreeScore.text = playerThree.score.ToString();
+        dungeonMasterScore.text = dungeonMaster.score.ToString();
+
+    }
+}
