b68e65efcab12a6ff4f5d653aadb660f0887a24b
Lachlan Jordon <lachlan.jordon@ad.aie.edu>
2016-04-12 11:04:02 +1000

    working on camera zoom/focus

--- Gamejam 2 - April 2016/Assets/Scripts/DungeonMaster.cs
+++ Gamejam 2 - April 2016/Assets/Scripts/DungeonMaster.cs
@@ -3,77 +3,135 @@ using System.Collections;
 
 public class DungeonMaster : MonoBehaviour
 {
-    public TrapPlaceholder one;
-    public TrapPlaceholder two;
-    public TrapPlaceholder three;
-    public TrapPlaceholder four;
-    public TrapPlaceholder five;
+    //public TrapPlaceholder one;
+    //public TrapPlaceholder two;
+    //public TrapPlaceholder three;
+    //public TrapPlaceholder four;
+    //public TrapPlaceholder five;
+
+    Transform cameraMaster;
+
+    public Transform playerOne;
+    public Transform playerTwo;
+    public Transform playerThree;
+
+    float pOneX;
+    float pOneY;
+
+    float pTwoX;
+    float pTwoY;
+
+    float pThreeX;
+    float pThreeY;
 
     // Use this for initialization
-    void Start ()
-    {
-            
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
     {
 
-        ActivateTraps();
+        cameraMaster = GetComponentInParent<Transform>();
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         UpdateCamera();
-        
+
+        pOneX = playerOne.position.x;
+        pOneY = playerOne.position.y;
+
+        pTwoX = playerTwo.position.x;
+        pTwoY = playerTwo.position.y;
+
+        pThreeX = playerThree.position.x;
+        pThreeY = playerThree.position.y;
     }
 
 
-    void ActivateTraps()
-    {
-        //if (Input.GetButton("joystick 1 button 0"))
-        if (Input.GetKeyDown("q"))
-        {
-            one.DoTheThing();
-        }
-
-        //if (Input.GetButton("joystick 1 button 1"))
-        if (Input.GetKeyDown("w"))
-        {
-            two.DoTheThing();
-        }
-
-        //if (Input.GetButton("joystick 1 button 2"))
-        if (Input.GetKeyDown("e"))
-        {
-            three.DoTheThing();
-        }
-
-        //if (Input.GetButton("joystick 1 button 3"))
-        if (Input.GetKeyDown("r"))
-        {
-            four.DoTheThing();
-        }
-
-        //if (Input.GetButton("joystick 1 button 4"))
-        if (Input.GetKeyDown("space"))
-        {
-            five.DoTheThing();
-        }
-    }
+    //void ActivateTraps()
+    //{
+    //    //if (Input.GetButton("joystick 1 button 0"))
+    //    if (Input.GetKeyDown("q"))
+    //    {
+    //        one.DoTheThing();
+    //    }
+    //
+    //    //if (Input.GetButton("joystick 1 button 1"))
+    //    if (Input.GetKeyDown("w"))
+    //    {
+    //        two.DoTheThing();
+    //    }
+    //
+    //    //if (Input.GetButton("joystick 1 button 2"))
+    //    if (Input.GetKeyDown("e"))
+    //    {
+    //        three.DoTheThing();
+    //    }
+    //
+    //    //if (Input.GetButton("joystick 1 button 3"))
+    //    if (Input.GetKeyDown("r"))
+    //    {
+    //        four.DoTheThing();
+    //    }
+    //
+    //    //if (Input.GetButton("joystick 1 button 4"))
+    //    if (Input.GetKeyDown("space"))
+    //    {
+    //        five.DoTheThing();
+    //    }
+    //}
 
 
     void UpdateCamera()
     {
-        if (Input.GetAxis("Fire3") != 0)
-        {
-            //transform.position = transform.position + Input.GetAxis("joystick 1 axis 5") * transform.forward;
-            transform.position = transform.position + Input.GetAxis("Vertical") * transform.forward;
-        }
-        else
-        {
-            //transform.position = transform.position + Input.GetAxis("joystick 1 axis y") * transform.up;
-            transform.position = transform.position + Input.GetAxis("Vertical") * transform.up;
-        }
+        //find average x value
+        float xAv = (pOneX + pTwoX + pThreeX) / 3;
 
-        //transform.position = transform.position + Input.GetAxis("joystick 1 axis x") * transform.right;
-        transform.position = transform.position + Input.GetAxis("Horizontal") * transform.right;
+        //find average y value
+        float yAv = (pOneY + pTwoY + pThreeY) / 3;
+
+        Vector3 newPos = new Vector3(xAv, yAv, cameraMaster.position.z);
+        cameraMaster.position = newPos;
+
+        //change the orthographic size value
+
+        //find highest/lowest x value
+
+        float hiX = 0;
+        float hiY = 0;
+        float loX = 0;
+        float loY = 0;
+
+        if (pOneX > pTwoX && pOneX > pThreeX)
+            hiX = pOneX;
+        else if (pTwoX > pOneX && pTwoX > pThreeX)
+            hiX = pTwoX;
+        else if (pThreeX > pOneX && pThreeX > pTwoX)
+            hiX = pThreeX;
+
+        if (pOneX < pTwoX && pOneX < pThreeX)
+            loX = pOneX;
+        else if (pTwoX < pOneX && pTwoX < pThreeX)
+            loX = pTwoX;
+        else if (pThreeX < pOneX && pThreeX < pTwoX)
+            loX = pThreeX;
+
+        //find highest y value
+
+        if (pOneY > pTwoY && pOneY > pThreeY)
+            hiY = pOneY;
+        else if (pTwoY > pOneY && pTwoY > pThreeY)
+            hiY = pTwoY;
+        else if (pThreeY > pOneY && pThreeY > pTwoY)
+            hiY = pThreeY;
+
+        if (pOneY < pTwoY && pOneY < pThreeY)
+            loY = pOneY;
+        else if (pTwoY < pOneY && pTwoY < pThreeY)
+            loY = pTwoY;
+        else if (pThreeY < pOneY && pThreeY < pTwoY)
+            loY = pThreeY;
+
+        GetComponent<Camera>().orthographicSize = (hiX - loX / hiY - loY) + 5;
     }
 }
