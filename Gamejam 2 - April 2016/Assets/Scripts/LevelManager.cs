ea96c47a4daa74d3fb4bb6ea346ecc0663a90b5f
Lachlan Jordon <lachlan.jordon@ad.aie.edu>
2016-04-13 10:23:44 +1000

    Loading next level on completion of current

--- Gamejam 2 - April 2016/Assets/Scripts/LevelManager.cs
+++ Gamejam 2 - April 2016/Assets/Scripts/LevelManager.cs
@@ -8,4 +8,27 @@ public class LevelManager{
 	{
 		SceneManager.LoadScene(Random.Range(1, SceneManager.sceneCount));
 	}
+
+    public static void LoadNextLevel()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+            SceneManager.LoadScene("Level2");
+
+        if (SceneManager.GetActiveScene().name == "Level2")
+            SceneManager.LoadScene("Level3");
+
+        if (SceneManager.GetActiveScene().name == "Level3")
+            SceneManager.LoadScene("Level4");
+
+        if (SceneManager.GetActiveScene().name == "Level4")
+            SceneManager.LoadScene("Level5");
+
+        if (SceneManager.GetActiveScene().name == "Level5")
+            SceneManager.LoadScene("Level6");
+
+        //instead of this last one we might want to make a 'win' scene
+
+        if (SceneManager.GetActiveScene().name == "Level6")
+            SceneManager.LoadScene("Level1");
+    }
 }
